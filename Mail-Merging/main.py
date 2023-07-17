5d1afe216c333ca13884ac50428ebc74753bd396
Nikhil Bhaladhare <nikbhaladhare2104@iitb.ac.in>
2023-07-17 13:34:45 +0530

    Add files via upload

--- Mail-Merging/main.py
+++ Mail-Merging/main.py
@@ -1,1 +1,24 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+NEW = "[name]"
+
+with open("./Input/Names/invited_names.txt") as f:
+    names = f.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as file:
+    contents = file.read()
+
+for name in names:
+    stripped_name = name.strip()
+    new_contents = contents.replace(NEW, stripped_name)
+    with open(f"./Output/ReadyToSend/letter_for_{stripped_name}", mode="w") as completed:
+        completed.write(new_contents)
+
+
 
