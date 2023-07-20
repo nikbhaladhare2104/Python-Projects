ee09b6e35053ffe843171bb9f0b5110cd71ca2cd
Nikhil Bhaladhare <nikbhaladhare2104@iitb.ac.in>
2023-07-20 12:45:25 +0530

    Add files via upload

--- Birthday-Wisher/main.py
+++ Birthday-Wisher/main.py
@@ -1,1 +1,13 @@
+import smtplib
 
+my_email = "nikbhaladhare2104@gmail.com"
+password = "sksgjywmkixlkcdd"
+
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="nihalsodhi112000@gmail.com",
+        msg="Subject:Hello\n\nThis is the body of the mail."
+    )
