7beeff0158391d64002aeabadcd666ab933bfee9
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2023-07-20 12:47:15 +0530

    Update main.py

--- Birthday-Wisher/main.py
+++ Birthday-Wisher/main.py
@@ -1,13 +1,13 @@
 import smtplib
 
-my_email = "nikbhaladhare2104@gmail.com"
-password = "sksgjywmkixlkcdd"
+my_email = "YOUR EMAIL ID"
+password = "PASSWORD"
 
 with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
     connection.starttls()
     connection.login(user=my_email, password=password)
     connection.sendmail(
         from_addr=my_email,
-        to_addrs="nihalsodhi112000@gmail.com",
+        to_addrs="RECEIVER'S MAIL ID",
         msg="Subject:Hello\n\nThis is the body of the mail."
     )
