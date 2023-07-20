87ffea12d4b79e11ae7657699e0a64a6d270f1bf
Nikhil Bhaladhare <nikbhaladhare2104@iitb.ac.in>
2023-07-20 12:49:05 +0530

    Add files via upload

--- API/ISS-Position/main.py
+++ API/ISS-Position/main.py
@@ -1,1 +1,50 @@
+import requests
+from datetime import datetime
+from twilio.rest import Client
 
+
+END = "https://api.sunrise-sunset.org/json"
+LAT = 19.075983
+LON = 72.877655
+
+def iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+
+    data = response.json()
+    longitude = float(data["iss_position"]["longitude"])
+    latitude = float(data["iss_position"]["latitude"])
+    iss_position = (longitude, latitude)
+    print(iss_position)
+
+    # checking the position of iss
+    if LAT-5 <= latitude <= LAT+5 and LON-5 <= longitude <= LON+5:
+        return True
+
+
+def night():
+    para = {
+        "lat": LAT,
+        "lon": LON,
+        "formatted": 0
+    }
+    response = requests.get(END, params=para)
+    data = response.json()["results"]
+    sunrise = int(data["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+if night() and iss_overhead():
+    account_sid = 'AC4b2be4a1fc89b24fdef109903e73a475'
+    auth_token = '99f2807450e29d36d92e7a63497f7f39'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body='ISS is Overhead. Go outside.',
+        to='whatsapp:+918208773377'
+    )
